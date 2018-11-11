cf9b6c4b6d645d7aee401552bdc67545f90bcdb7
Borislav Kosun <kosun.b@yandex.ua>
2018-11-12 01:27:34 +0200

    Install MySQLi database driver as default driver

--- modules/database/config/database.php
+++ modules/database/config/database.php
@@ -2,32 +2,35 @@
 
 return array
 (
-	'default' => array
-	(
-		'type'       => 'MySQL',
-		'connection' => array(
+ 	'default' => array
+ 	(
+ 		'type'       => 'MySQLi',
+ 		'connection' => array(
 			/**
-			 * The following options are available for MySQL:
+			 * The following options are available for MySQLi:
 			 *
 			 * string   hostname     server hostname, or socket
 			 * string   database     database name
 			 * string   username     database username
 			 * string   password     database password
 			 * boolean  persistent   use persistent connections?
+			 * array    ssl          ssl parameters as "key => value" pairs.
+			 *                       Available keys: client_key_path, client_cert_path, ca_cert_path, ca_dir_path, cipher
 			 * array    variables    system variables as "key => value" pairs
 			 *
 			 * Ports and sockets may be appended to the hostname.
 			 */
-			'hostname'   => 'localhost',
-			'database'   => 'kohana',
-			'username'   => FALSE,
-			'password'   => FALSE,
-			'persistent' => FALSE,
-		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-	),
+ 			'hostname'   => 'localhost',
+ 			'database'   => 'kohana',
+ 			'username'   => FALSE,
+ 			'password'   => FALSE,
+ 			'persistent' => FALSE,
+ 			'ssl'        => NULL,
+ 		),
+ 		'table_prefix' => '',
+ 		'charset'      => 'utf8',
+ 		'caching'      => FALSE,
+ 	),
 	'alternate' => array(
 		'type'       => 'PDO',
 		'connection' => array(
@@ -53,38 +56,4 @@ return array
 		'charset'      => 'utf8',
 		'caching'      => FALSE,
 	),
-	/**
-	 * MySQLi driver config information
-	 *
-	 * The following options are available for MySQLi:
-	 *
-	 * string   hostname     server hostname, or socket
-	 * string   database     database name
-	 * string   username     database username
-	 * string   password     database password
-	 * boolean  persistent   use persistent connections?
-	 * array    ssl          ssl parameters as "key => value" pairs.
-	 *                       Available keys: client_key_path, client_cert_path, ca_cert_path, ca_dir_path, cipher
-	 * array    variables    system variables as "key => value" pairs
-	 *
-	 * Ports and sockets may be appended to the hostname.
-	 *
-	 * MySQLi driver config example:
-	 *
-	 */
-// 	'alternate_mysqli' => array
-// 	(
-// 		'type'       => 'MySQLi',
-// 		'connection' => array(
-// 			'hostname'   => 'localhost',
-// 			'database'   => 'kohana',
-// 			'username'   => FALSE,
-// 			'password'   => FALSE,
-// 			'persistent' => FALSE,
-// 			'ssl'        => NULL,
-// 		),
-// 		'table_prefix' => '',
-// 		'charset'      => 'utf8',
-// 		'caching'      => FALSE,
-// 	),
 );
