81197023226d644bd4efd0fe3f68df49142d6d0b
Borislav Kosun <kosun.b@yandex.ua>
2022-10-27 03:28:01 +0300

    refactor: use a single empty class declaration style

--- modules/orm/classes/Model/Role.php
+++ modules/orm/classes/Model/Role.php
@@ -1,7 +1,3 @@
 <?php
 
-class Model_Role extends Model_Auth_Role {
-
-	// This class can be replaced or extended
-
-} // End Role Model
+class Model_Role extends Model_Auth_Role {}
--- modules/orm/classes/Model/User/Token.php
+++ modules/orm/classes/Model/User/Token.php
@@ -1,7 +1,3 @@
 <?php
 
-class Model_User_Token extends Model_Auth_User_Token {
-
-	// This class can be replaced or extended
-
-} // End User Token Model
+class Model_User_Token extends Model_Auth_User_Token {}
