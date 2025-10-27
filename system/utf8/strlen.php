ca807ac568383ca9337a08f87a6ab32611d5a847
Borislav Kosun <16259603+bkosun@users.noreply.github.com>
2025-10-27 23:56:12 +0200

    chore: do not use deprecated utf8_decode function (#123)

--- system/utf8/strlen.php
+++ system/utf8/strlen.php
@@ -13,5 +13,27 @@ function _strlen($str)
 	if (UTF8::is_ascii($str))
 		return strlen($str);
 
-	return strlen(utf8_decode($str));
+	// https://php.watch/versions/8.2/utf8_encode-utf8_decode-deprecated#utf8_decode-php
+	// https://github.com/symfony/polyfill-php72/blob/v1.26.0/Php72.php#L40-65
+	for ($i = 0, $j = 0; $i < strlen($str); ++$i, ++$j) {
+		switch ($str[$i] & "\xF0")
+		{
+			case "\xC0":
+			case "\xD0":
+				$c       = (ord($str[$i] & "\x1F" ) << 6) | ord($str[++$i] & "\x3F");
+				$str[$j] = $c < 256 ? chr($c) : '?';
+			break;
+			case "\xF0":
+				++$i;
+				// no break
+			case "\xE0":
+				$str[$j] = '?';
+				$i      += 2;
+			break;
+			default:
+				$str[$j] = $str[$i];
+		}
+	}
+
+	return strlen(substr($str, 0, $j));
 }
